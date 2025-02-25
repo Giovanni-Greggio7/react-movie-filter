bf7bb92054c8f17d88d0d71d8b5c11b346c21fe2
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-02-25 15:55:03 +0100

    Add: First bonus

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ function App() {
 
   const [movie, setMovie] = useState(Movie)
 
+  const [newMovie, setNewMovie] = useState('')
+
   const [search, setSearch] = useState('')
   const [filteredMovies, setFilteredMovies] = useState(movie)
+
+  const addMovie = (e) => {
+    e.preventDefault();
+    const movies = newMovie.trim()
+    setMovie([...movie, movies])
+    setNewMovie('')
+  }
 
   useEffect(() => {
     setFilteredMovies(
@@ -27,10 +36,12 @@ function App() {
         <h1>Che film vuoi guardare oggi?</h1>
         <h3>Seleziona la categoria!</h3>
 
-        <select className='form-select' 
-        value={search} 
-        onChange={ (e) => setSearch(e.target.value)}
+        <select className='form-select'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         >
+
+          {/* Provare a usare il map */}
           <option value="" disabled>Seleziona un'opzione</option>
           <option value="Fantascienza">Fantascienza</option>
           <option value="Thriller">Thriller</option>
@@ -38,13 +49,33 @@ function App() {
           <option value="Azione">Azione</option>
         </select>
 
+        <input type="text"
+          className='form-control'
+          placeholder='Che film vorresti vedere?'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
         <ul className='list-group'>
-          {filteredMovies.map(({id,title}) => (
+          {filteredMovies.map(({ id, title }) => (
             <li className='list-group-item fs-3' key={id}>
               {title}
             </li>
           ))}
         </ul>
+
+        <form onSubmit={addMovie}>
+          <input type="text"
+            className='form-control'
+            placeholder='Aggiungi film'
+            value={newMovie}
+            onChange={(e) => setNewMovie(e.target.value)} />
+        </form>
+        <button type="submit" className="btn btn-primary mt-2">
+          Aggiungi
+        </button>
+
+
 
       </div>
     </>
